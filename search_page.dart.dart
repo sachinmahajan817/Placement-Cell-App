c913312169d1d0ee81cf90cc0c23e72afb565f13
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2023-05-23 17:52:49 +0530

    Add files via upload

---
+++ search_page.dart.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:tnp/screens/search/widgets/search_app_bar.dart';
+import 'package:tnp/screens/search/widgets/search_input.dart';
+import 'package:tnp/screens/search/widgets/search_list.dart';
+import 'package:tnp/screens/search/widgets/search_option.dart';
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.grey.withOpacity(0.1),
+                    ))
+              ],
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SearchAppBar(),
+                //    SearchInput(),
+                //SearchOption(),
+                Expanded(child: SearchList()),
+              ],
+            )
+          ],
+        ));
+  }
+}

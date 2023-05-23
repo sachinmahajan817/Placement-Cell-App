edd189bb5459541ec7acbfccda26208682014639
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2023-05-23 17:50:21 +0530

    Add files via upload

---
+++ sample3.dart.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tnp/widgets/themes.dart';
+
+class Sample3 extends StatelessWidget {
+  const Sample3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Sample Resume 3',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0.00,
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(5),
+              // width: MediaQuery.of(context).size.width,
+              // height: MediaQuery.of(context).size.height,
+              width: 400,
+              height: 500,
+              child: Hero(
+                tag: 'Sample 1',
+                child: Image.asset(
+                  'assets/images/sample3.png',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

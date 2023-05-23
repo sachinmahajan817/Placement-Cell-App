923ab15ddb39ad79a36ee611f4017559675193f0
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2023-05-23 17:34:56 +0530

    Add files via upload

---
+++ job.dart.dart
@@ -0,0 +1,21 @@
+class Job {
+  String companyName;
+  String jobProfile;
+  String salary;
+  String jobType;
+  String deadline;
+  String link;
+  String description;
+  String docId;
+
+  Job(
+    this.companyName,
+    this.jobProfile,
+    this.salary,
+    this.jobType,
+    this.deadline,
+    this.link,
+    this.description,
+    this.docId,
+  );
+}

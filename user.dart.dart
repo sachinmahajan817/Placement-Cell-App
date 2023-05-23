9033788a0fa9a40ece54175c19983727cd2c84df
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2023-05-23 17:56:14 +0530

    Add files via upload

---
+++ user.dart.dart
@@ -0,0 +1,19 @@
+class User {
+  String name = "";
+  String phone = "";
+  String email = "";
+  String password = "";
+  String rollno = "";
+  String year = "";
+  String branch = "";
+
+  User(
+    this.name,
+    this.phone,
+    this.email,
+    this.password,
+    this.rollno,
+    this.year,
+    this.branch,
+  );
+}
